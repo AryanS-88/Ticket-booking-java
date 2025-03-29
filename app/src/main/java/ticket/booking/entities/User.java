830446e75a9b4d88c05243dd201b73b719a29863
AryanS-88 <ssingharyan70@gmail.com>
2025-03-29 16:43:08 +0530

    project entities

---
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -0,0 +1,9 @@
+package ticket.booking.entities;
+import java.util.List;
+
+public class User {
+    private String name;
+    private String Password;
+    private String HashPassword;
+    private List<Ticket> ticketsBooked
+}
