1a60ad8804ef08d39097f0dbe1343f4d27d6f3e4
AryanS-88 <ssingharyan70@gmail.com>
2025-03-30 23:59:02 +0530

    services

--- app/src/main/java/ticket/booking/App.java
+++ app/src/main/java/ticket/booking/App.java
@@ -3,10 +3,12 @@
  */
 package ticket.booking;
 
+import java.util.Arrays;
+
 public class App {
 
 
     public static void main(String[] args) {
-
+        List<Integer> l = Arrays.asList(1,2,3,4,5,6,7,8,9)
     }
 }
